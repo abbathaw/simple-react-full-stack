6f090271412d44b6af914439d15998277e709728
Abdullah <aalthawr@gmail.com>
2020-01-13 17:03:36 +0800

    Initial Commit

---
+++ src/client/App.js
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from "react";
+import './app.css';
+import ReactImage from './react.png';
+
+const App =() => {
+  const [username, setUsername] = useState(null);
+
+  useEffect(()=> {
+    fetch('/api/getUsername')
+    .then(res => res.json())
+    .then(user => setUsername(user.username));
+  }, []);
+
+  return (
+        <div>
+          {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
+          <img src={ReactImage} alt="react" />
+        </div>
+  )
+};
+
+export default App;
